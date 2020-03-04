b672a21e3436995e2248d10c9e5faa024cd86db2
p-robot <will@probert.co.nz>
2020-03-04 18:39:00 +0000

    Add mock python/R files for plotting

---
+++ R/plotting.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+# 
+# Functions for plotting output of COVID19-IBM
+
+require(ggplot2)
+
---
+++ python/plotting.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Functions for plotting output from COVID19 in Python
+"""
+
+import numpy as np, pandas as pd
+from matplotlib import pyplot as plt
+
