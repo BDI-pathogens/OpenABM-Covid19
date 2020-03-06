ad9790cfe1acd37ca71c05e60eb31afaa29d0166
p-robot <will@probert.co.nz>
2020-03-06 14:49:05 +0000

    Remove printing used for testing

--- src/input.c
+++ src/input.c
@@ -12,37 +12,30 @@
 #include "params.h"
 #include "utilities.h"
 
-#define FILENAME_PARAM "parameters.csv"
 
+/*****************************************************************************************
+*  Name:		read_param_file
+*  Description: Read parameter file (csv), attach parameter values to params structure
+******************************************************************************************/
 
-void read_param_file(){
+void read_param_file( parameters *params ){
 	
 	FILE *parameter_file;
-
+	int check;
+	
 	parameter_file = fopen(FILENAME_PARAM, "r");
 	if(parameter_file == NULL){
 		print_exit("Can't open parameter file");
 	}
-	printf("Found parameter file\n");
 	
 	// Throw away header
 	fscanf(parameter_file, "%*[^\n]\n");
 	
-	int check;
-	long param_id, n_total, mean_daily_interactions, days_of_interactions, end_time;
-	
-	check = fscanf(parameter_file, "%ld ", &param_id);
-	check = fscanf(parameter_file, "%ld ", &n_total);
-	check = fscanf(parameter_file, "%ld ", &(mean_daily_interactions));
-	check = fscanf(parameter_file, "%ld ", &(days_of_interactions));
-	check = fscanf(parameter_file, "%ld", &(end_time));
-	
-	
-	printf("param_id: %ld\n", param_id);
-	printf("n_total: %ld\n", n_total);
-	printf("mean_daily_interactions: %ld\n", mean_daily_interactions);
-	printf("days_of_interactions: %ld\n", days_of_interactions);
-	printf("end_time: %ld\n", end_time);
-	
+	// Read and attach parameter values to parameter structure
+	check = fscanf(parameter_file, " %li ,", &(params->param_id));
+	check = fscanf(parameter_file, " %li ,", &(params->n_total));
+	check = fscanf(parameter_file, " %i ,", &(params->mean_daily_interactions));
+	check = fscanf(parameter_file, " %i ,", &(params->days_of_interactions));
+	check = fscanf(parameter_file, " %i", &(params->end_time));
 	fclose(parameter_file);
 }
