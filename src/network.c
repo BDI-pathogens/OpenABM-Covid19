3d1b5d28484ac866cef4093afda012b19d7b8f5b
p-robot <will@probert.co.nz>
2020-03-12 18:03:26 +0000

    Update number of edges per node in watts_strogatz_network

--- src/network.c
+++ src/network.c
@@ -10,6 +10,7 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_cdf.h>
+#include <math.h>
 
 #include "network.h"
 #include "utilities.h"
@@ -45,7 +46,7 @@ network* new_network(long n_total)
 void build_watts_strogatz_network( )
 {
 	long k = 10;
-	long N = 1000;
+	long N = 10000;
 	double p_rewire = 0.1;
 	
 	long incr = k/2, neighbour, i, j, l;
@@ -55,16 +56,17 @@ void build_watts_strogatz_network( )
 	printf("p_rewire : %f\n", p_rewire);
 	
 	// Allocate memory (needed for large N)
-    long** edge_mat;
+	long** edge_mat;
 	edge_mat = calloc(N, sizeof(long *));
-    for(i = 0; i < N; i++)
-        edge_mat[i] = calloc(k, sizeof(long));
+	for(i = 0; i < N; i++)
+		edge_mat[i] = calloc(k*10, sizeof(long));
 	
 	// Degree for each individual
-	int* n_edges_arr = calloc(N,  sizeof(int));
-	for(i = 0; i < N; i++)
+	int* n_edges_arr = calloc(N, sizeof(int));
+	for(i = 0; i < N; i++){
 		n_edges_arr[i] = k;
-	
+	}
+
 	// Step 1: Set up random lattice
 	for(i = 0; i < N; i++){
 		j = 0; l = 0;
@@ -82,6 +84,7 @@ void build_watts_strogatz_network( )
 	
 	double u;
 	long new_contact, old_contact;
+	
 	// Step 2: Randomly rewire connections with probability "p_rewire"
 	for(i = 0; i < N; i++){
 		for(j = 0; j < n_edges_arr[i]; j++){
@@ -90,33 +93,37 @@ void build_watts_strogatz_network( )
 			
 			if(u < p_rewire){
 				
-				new_contact = i;
+				// Draw a new contact (long between 0 and N-1)
+				new_contact = (long) floor(gsl_rng_uniform(rng) * N);
 				
 				// Check if new_connection is already connected (or is self)
 				while(check_member_or_self(new_contact, i, edge_mat[i], n_edges_arr[i])){
 					new_contact = gsl_rng_uniform_int(rng, N);
 				}
 				
+				// Remove contact between person "i" and the original contact
 				old_contact = edge_mat[i][j];
+				
+				remove_contact(edge_mat[old_contact], i, &(n_edges_arr[old_contact]));
 				
 				// if not, rewire to this new contact
 				edge_mat[i][j] = new_contact;
 				
-				// rewire other person too
-				remove_contact(edge_mat[old_contact], i, &(n_edges_arr[old_contact]));
+				// Add i to list of connections to new_contact (and increase number of edges)
+				add_contact(edge_mat[new_contact], i, &(n_edges_arr[new_contact]));
 			}
 		}
 	}
-	
 	// Count total edges (i.e. network->n_edges)
 	long n_edges = 0;
 	for(i = 0; i < N; i++){
 		n_edges += n_edges_arr[i];
 	}
-	
-	// Form array of total edges (i.e. network->edges)
+
+	// // Form array of total edges (i.e. network->edges)
 	edge* edges;
 	edges = calloc(n_edges, sizeof(edge));
+
 	long idx = 0;
 	for(i = 0; i < N; i++){
 		for(j = 0; j < n_edges_arr[i]; j++){
@@ -125,14 +132,12 @@ void build_watts_strogatz_network( )
 			idx++;
 		}
 	}
-	
-    for(i = 0; i < N; i++)
-        free(edge_mat[i]);
-	free(edge_mat);
-	free(n_edges_arr);
-	
 	free(edges);
 	
+	for(i = 0; i < N; i++)
+		free(edge_mat[i]);
+	free(edge_mat);
+	free(n_edges_arr);
 }
 
 
@@ -141,17 +146,29 @@ void build_watts_strogatz_network( )
 *  Description: Remove a contact from a list of edges, tidy list
 ******************************************************************************************/
 
-void remove_contact(long *current_contacts, long contact_to_remove, int *n_edges){
+void remove_contact(long *current_contacts_arr, long contact_to_remove, int *length){
+	
 	int i, j = 0;
-	for(i = 0; i < *n_edges; i++){
-		
-		if(current_contacts[i] != contact_to_remove){
-			current_contacts[j] = current_contacts[i];
+	
+	for(i = 0; i < *length; i++){
+		if(current_contacts_arr[i] != contact_to_remove){
+			current_contacts_arr[j] = current_contacts_arr[i];
 			j++;
 		}
 	}
-	current_contacts[j] = UNKNOWN;
-	--*n_edges;
+	current_contacts_arr[j] = UNKNOWN;
+	--*length;
+}
+
+/*****************************************************************************************
+*  Name:		add_contact
+*  Description: Add a contact from a list of edges
+******************************************************************************************/
+
+void add_contact(long *current_contacts_arr, long contact_to_add, int *length){
+	
+	current_contacts_arr[*length] = contact_to_add;
+	++*length;
 }
 
 
