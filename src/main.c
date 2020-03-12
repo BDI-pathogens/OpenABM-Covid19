ac3b5123d98576f2219f7d60e024c543838dddd6
p-robot <will@probert.co.nz>
2020-03-12 18:41:26 +0000

    Add build_watts_strogatz_network() call

--- src/main.c
+++ src/main.c
@@ -40,7 +40,9 @@ int main(int argc, char *argv[])
 	gsl_rng_set( rng, params.rng_seed );
 	model *model = new_model( &params );
 	
-	build_watts_strogatz_network();
+	
+	//network *household_network = new_network( params.n_total );
+	//build_watts_strogatz_network( household_network, &params );
 	
 	//setup_output_files( model, &params );
 
