b9ea3682bbff70b0ab8311d4147f29aa3320a16b
p-robot <will@probert.co.nz>
2020-03-12 13:06:26 +0000

    Infrastructure for watts strogatz model

--- src/network.c
+++ src/network.c
@@ -14,7 +14,6 @@
 #include "network.h"
 #include "utilities.h"
 #include "constant.h"
-#include "params.h"
 
 /*****************************************************************************************
 *  Name:		new_network
@@ -37,6 +36,61 @@ network* new_network(long n_total)
 }
 
 
+/*****************************************************************************************
+*  Name:		watts_strogatz_network
+*  Description: Build a Watts Strogatz network
+*  Returns:		pointer to model
+******************************************************************************************/
+
+void build_watts_strogatz_network()
+{
+	
+	// Output: network->n_edges
+	// Output: network->edges
+	
+	long k = 10;
+	long N = 1000;
+	double p_rewire = 0.1;
+	
+	long incr = k/2, neighbour, i, j, l;
+	
+	printf("k : %li\n", k);
+	printf("N : %li\n", N);
+	printf("p_rewire : %f\n", p_rewire);
+	
+	// Allocate memory (needed for large N)
+    long** edges = calloc(N,  sizeof(long*));
+    for(i = 0; i < N; i++)
+        edges[i] = calloc(k, sizeof(long));
+	
+	// Step 1: Set up random lattice
+	
+	for(i = 0; i < N; i++){
+		j = 0; l = 0;
+		while(l < k)
+		{
+			// Make sure we loop to the start of the ring
+			neighbour = (i - incr + j + N) % N;
+			
+			if(neighbour != i){
+				edges[i][l] = neighbour;
+				l++;
+			}
+			j++;
+		}
+	}
+	
+	
+	for(i = 0; i < N; i++){
+		printf("%li ", i);
+		for(j = 0; j < k; j++){
+				printf("%li ", edges[i][j]);
+		}
+		printf("\n");
+	}
+	
+	// Step 2: Randomly rewire connections
+}
 
 /*****************************************************************************************
 *  Name:		destroy_network
