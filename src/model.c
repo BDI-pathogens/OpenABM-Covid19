4b60523f7d8db4579441252e668f3142e5dea68b
roberthinch <robert.hinch@gmail.com>
2020-03-15 19:19:28 +0000

    Label interactions by the type of network they came from when adding

--- src/model.c
+++ src/model.c
@@ -1140,11 +1140,13 @@ void add_interactions_from_network(
 		inter1 = &(model->interactions[ all_idx++ ]);
 		inter2 = &(model->interactions[ all_idx++ ]);
 
+		inter1->type       = network->type;
 		inter1->individual = indiv2;
 		inter1->next       = indiv1->interactions[ day ];
 		indiv1->interactions[ day ] = inter1;
 		indiv1->n_interactions[ day ]++;
 
+		inter2->type       = network->type;
 		inter2->individual = indiv1;
 		inter2->next       = indiv2->interactions[ day ];
 		indiv2->interactions[ day ] = inter2;
