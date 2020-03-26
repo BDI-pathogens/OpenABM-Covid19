b1ae6c59a260d63d4f2259b5ed8bec40e00d9263
p-robot <will@probert.co.nz>
2020-03-26 15:31:41 +0000

    Test: add script for testing of data within the individual file

---
+++ tests/test_individual.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+Tests of the individual-based model, COVID19-IBM, using the individual file
+
+Usage:
+With pytest installed (https://docs.pytest.org/en/latest/getting-started.html) tests can be 
+run by calling 'pytest' from project folder.  
+
+Created: March 2020
+Author: p-robot
+"""
+
+import subprocess, shutil, os
+from os.path import join
+import numpy as np, pandas as pd
+import pytest
+
+from parameters import ParameterSet
+import utilities as utils
+
+# Directories
+IBM_DIR = "src"
+IBM_DIR_TEST = "src_test"
+DATA_DIR_TEST = "data_test"
+
+TEST_DATA_TEMPLATE = "./tests/data/baseline_parameters.csv"
+TEST_DATA_FILE = join(DATA_DIR_TEST, "test_parameters.csv")
+
+TEST_OUTPUT_FILE = join(DATA_DIR_TEST, "test_output.csv")
+TEST_INDIVIDUAL_FILE = join(DATA_DIR_TEST, "individual_file_Run1.csv")
+
+TEST_HOUSEHOLD_TEMPLATE = "./tests/data/baseline_household_demographics.csv"
+TEST_HOUSEHOLD_FILE = join(DATA_DIR_TEST, "test_household_demographics.csv")
+
+PARAM_LINE_NUMBER = 1
+
+# Construct the executable command
+EXE = "covid19ibm.exe {} {} {} {}".format(TEST_DATA_FILE,
+                                       PARAM_LINE_NUMBER,
+                                       DATA_DIR_TEST, 
+                                       TEST_HOUSEHOLD_FILE)
+
+command = join(IBM_DIR_TEST, EXE)
+
+
+class TestClass(object):
+    """
+    Test class for checking 
+    """
+    @classmethod
+    def setup_class(self):
+        """
+        When the class is instantiated: compile the IBM in a temporary directory
+        """
+        
+        # Make a temporary copy of the code (remove this temporary directory if it already exists)
+        shutil.rmtree(IBM_DIR_TEST, ignore_errors = True)
+        shutil.copytree(IBM_DIR, IBM_DIR_TEST)
+                
+        # Construct the compilation command and compile
+        compile_command = "make clean; make all"
+        completed_compilation = subprocess.run([compile_command], 
+            shell = True, cwd = IBM_DIR_TEST, capture_output = True)
+    
+    @classmethod
+    def teardown_class(self):
+        """
+        Remove the temporary code directory (when this class is removed)
+        """
+        shutil.rmtree(IBM_DIR_TEST, ignore_errors = True)
+    
+    def setup_method(self):
+        """
+        Called before each method is run; creates a new data dir, copies test datasets
+        """
+        os.mkdir(DATA_DIR_TEST)
+        shutil.copy(TEST_DATA_TEMPLATE, TEST_DATA_FILE)
+        shutil.copy(TEST_HOUSEHOLD_TEMPLATE, TEST_HOUSEHOLD_FILE)
+        
+        # Adjust any parameters that need adjusting for all tests
+        params = ParameterSet(TEST_DATA_FILE, line_number = 1)
+        params.set_param("n_total", 10000)
+        params.set_param("end_time", 100)
+        params.write_params(TEST_DATA_FILE)
+        
+    def teardown_method(self):
+        """
+        At the end of each method (test), remove the directory of test input/output data
+        """
+        shutil.rmtree(DATA_DIR_TEST, ignore_errors = True)
+        
+    def test_file_exists(self):
+        """
+        Test that the individual file exists
+        """
+        
+        # Call the model using baseline parameters, pipe output to file, read output file
+        file_output = open(TEST_OUTPUT_FILE, "w")
+        completed_run = subprocess.run([command], stdout = file_output, shell = True)
+        
+        df_output = pd.read_csv(TEST_OUTPUT_FILE, comment = "#", sep = ",")
+        df_individual = pd.read_csv(TEST_INDIVIDUAL_FILE, comment = "#", sep = ",")
+        
+        np.testing.assert_equal(df_individual.shape[0] > 1, True)
+
+
