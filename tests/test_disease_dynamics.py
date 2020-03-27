71319762b687d66235c25eb818f9bb60e3d818ea
roberthinch <robert.hinch@gmail.com>
2020-03-27 00:14:46 +0000

    First cut at adding test disease outcome fraction Add age_group to the individual file to test the age structure of the disease outcome

--- tests/test_disease_dynamics.py
+++ tests/test_disease_dynamics.py
@@ -18,6 +18,8 @@ import pytest
 from parameters import ParameterSet
 import utilities as utils
 from math import sqrt
+#from test.test_bufio import lengths
+#from CoreGraphics._CoreGraphics import CGRect_getMidX
 
 # Directories
 IBM_DIR = "src"
@@ -32,6 +34,19 @@ TEST_INDIVIDUAL_FILE = join(DATA_DIR_TEST, "individual_file_Run1.csv")
 
 TEST_HOUSEHOLD_TEMPLATE = "./tests/data/baseline_household_demographics.csv"
 TEST_HOUSEHOLD_FILE = join(DATA_DIR_TEST, "test_household_demographics.csv")
+
+# Age groups
+AGE_0     = 0
+AGE_10_19 = 1
+AGE_20_29 = 2
+AGE_30_39 = 3
+AGE_40_49 = 4
+AGE_50_59 = 5
+AGE_60_69 = 6
+AGE_70_79 = 7
+AGE_80    = 8
+AGES = [ AGE_0, AGE_10_19, AGE_20_29, AGE_30_39, AGE_40_49, AGE_50_59, AGE_60_69, AGE_70_79, AGE_80 ]
+
 
 PARAM_LINE_NUMBER = 1
 
@@ -117,20 +132,70 @@ class TestClass(object):
                 sd_time_to_death      = 6,
                 mean_asymptomatic_to_recovery = 14.0,
                 sd_asymptomatic_to_recovery   = 5.0
+            )
+        ] ,
+        "test_disease_outcome_proportions" : [
+            dict(
+                fraction_asymptomatic       = 0.05,
+                hospitalised_fraction_0_9   = 0.05,
+                hospitalised_fraction_10_19 = 0.10,
+                hospitalised_fraction_20_29 = 0.10,
+                hospitalised_fraction_30_39 = 0.20,
+                hospitalised_fraction_40_49 = 0.20,
+                hospitalised_fraction_50_59 = 0.20,
+                hospitalised_fraction_60_69 = 0.30,
+                hospitalised_fraction_70_79 = 0.30,
+                hospitalised_fraction_80    = 0.50
             ),
             dict(
-                mean_time_to_symptoms = 6.5,
-                sd_time_to_symptoms   = 3.0,
-                mean_time_to_hospital = 1.6,
-                mean_time_to_critical = 2.0,
-                mean_time_to_recover  = 10.0,
-                sd_time_to_recover    = 5.0,
-                mean_time_to_death    = 16.0,
-                sd_time_to_death      = 4,
-                mean_asymptomatic_to_recovery = 13.0,
-                sd_asymptomatic_to_recovery   = 4.5
-            )
-        ] 
+                fraction_asymptomatic       = 0.15,
+                hospitalised_fraction_0_9   = 0.50,
+                hospitalised_fraction_10_19 = 0.40,
+                hospitalised_fraction_20_29 = 0.30,
+                hospitalised_fraction_30_39 = 0.20,
+                hospitalised_fraction_40_49 = 0.20,
+                hospitalised_fraction_50_59 = 0.20,
+                hospitalised_fraction_60_69 = 0.30,
+                hospitalised_fraction_70_79 = 0.30,
+                hospitalised_fraction_80    = 0.20
+            ),
+            dict(
+                fraction_asymptomatic       = 0.35,
+                hospitalised_fraction_0_9   = 0.05,
+                hospitalised_fraction_10_19 = 0.05,
+                hospitalised_fraction_20_29 = 0.05,
+                hospitalised_fraction_30_39 = 0.20,
+                hospitalised_fraction_40_49 = 0.20,
+                hospitalised_fraction_50_59 = 0.20,
+                hospitalised_fraction_60_69 = 0.30,
+                hospitalised_fraction_70_79 = 0.80,
+                hospitalised_fraction_80    = 0.90
+            ),
+            dict(
+                fraction_asymptomatic       = 0.45,
+                hospitalised_fraction_0_9   = 0.02,
+                hospitalised_fraction_10_19 = 0.02,
+                hospitalised_fraction_20_29 = 0.02,
+                hospitalised_fraction_30_39 = 0.10,
+                hospitalised_fraction_40_49 = 0.15,
+                hospitalised_fraction_50_59 = 0.20,
+                hospitalised_fraction_60_69 = 0.25,
+                hospitalised_fraction_70_79 = 0.30,
+                hospitalised_fraction_80    = 0.50
+            ),
+            dict(
+                fraction_asymptomatic       = 0.55,
+                hospitalised_fraction_0_9   = 0.20,
+                hospitalised_fraction_10_19 = 0.20,
+                hospitalised_fraction_20_29 = 0.20,
+                hospitalised_fraction_30_39 = 0.20,
+                hospitalised_fraction_40_49 = 0.20,
+                hospitalised_fraction_50_59 = 0.20,
+                hospitalised_fraction_60_69 = 0.20,
+                hospitalised_fraction_70_79 = 0.20,
+                hospitalised_fraction_80    = 0.20
+            ),
+        ]
         }
     """
     Test class for checking 
@@ -294,4 +359,80 @@ class TestClass(object):
         np.testing.assert_allclose( mean, mean_asymptomatic_to_recovery, atol = std_error_limit * sd / sqrt( N ) )
         np.testing.assert_allclose( sd, sd_asymptomatic_to_recovery, atol = std_error_limit * sd / sqrt( N ) )
      
+    def test_disease_outcome_proportions(
+            self,
+            fraction_asymptomatic,
+            hospitalised_fraction_0_9,
+            hospitalised_fraction_10_19,
+            hospitalised_fraction_20_29 ,
+            hospitalised_fraction_30_39,
+            hospitalised_fraction_40_49,
+            hospitalised_fraction_50_59,
+            hospitalised_fraction_60_69,
+            hospitalised_fraction_70_79,
+            hospitalised_fraction_80,
+        ):
+        """
+        Test that the fraction of infected people following each path for 
+        the progression of the disease agrees with the parameters
+        """
+        std_error_limit = 3
+        
+        params = ParameterSet(TEST_DATA_FILE, line_number = 1)
+        params.set_param("n_total", 20000)
+        params.set_param("n_seed_infection",200)
+        params.set_param("end_time", 50)
+        params.set_param("infectious_rate", 4.0)  
+        params.set_param("fraction_asymptomatic",fraction_asymptomatic)
+        params.set_param("hospitalised_fraction_0_9",   hospitalised_fraction_0_9)  
+        params.set_param("hospitalised_fraction_10_19", hospitalised_fraction_10_19)  
+        params.set_param("hospitalised_fraction_20_29", hospitalised_fraction_20_29)  
+        params.set_param("hospitalised_fraction_30_39", hospitalised_fraction_30_39)  
+        params.set_param("hospitalised_fraction_40_49", hospitalised_fraction_40_49)  
+        params.set_param("hospitalised_fraction_50_59", hospitalised_fraction_50_59)  
+        params.set_param("hospitalised_fraction_60_69", hospitalised_fraction_60_69)  
+        params.set_param("hospitalised_fraction_70_79", hospitalised_fraction_70_79)  
+        params.set_param("hospitalised_fraction_80",    hospitalised_fraction_80)  
+
+        
+        hospitalised_fraction = [ hospitalised_fraction_0_9,   hospitalised_fraction_10_19, hospitalised_fraction_20_29,
+                                  hospitalised_fraction_30_39, hospitalised_fraction_40_49, hospitalised_fraction_50_59,
+                                  hospitalised_fraction_60_69, hospitalised_fraction_70_79, hospitalised_fraction_80 ]
+        
+        params.write_params(TEST_DATA_FILE)        
+        file_output = open(TEST_OUTPUT_FILE, "w")
+        completed_run = subprocess.run([command], stdout = file_output, shell = True)
+        df_indiv = pd.read_csv(TEST_INDIVIDUAL_FILE, comment = "#", sep = ",", skipinitialspace = True )
+        
+         # fraction asymptomatic vs symptomatc
+        N_asym = len( df_indiv[ ( df_indiv['time_infected'] > 0 ) & ( df_indiv['time_asymptomatic'] > 0 ) ] )
+        N_sym  = len( df_indiv[ ( df_indiv['time_infected'] > 0 ) & ( df_indiv['time_asymptomatic'] < 0 ) ] )
+        N      = N_sym + N_asym
+        mean   = N_asym / N
+        sd     = sqrt( mean * ( 1 - mean ) )
+        np.testing.assert_allclose( mean, fraction_asymptomatic, atol = std_error_limit * sd / sqrt( N ) )
+      
+        # hospitalised fraction by age
+        N_hosp_tot = 0
+        N_symp_tot = 0
+        hospitalised_fraction_weighted = 0
+        for idx in range( len( AGES ) ):
+            
+            N_hosp = len( df_indiv[ ( df_indiv['time_hospitalised'] > 0 ) & ( df_indiv['age_group'] == AGES[idx] ) ] )
+            N_symp = len( df_indiv[ ( df_indiv['time_symptomatic'] >  0 ) & ( df_indiv['age_group'] == AGES[idx] ) ] )
+            mean   = N_hosp / N_symp
+            sd     = sqrt( mean * ( 1 - mean ) )
+            np.testing.assert_allclose( mean, hospitalised_fraction[idx], atol = std_error_limit * sd / sqrt( N_symp ) )
+            
+            N_hosp_tot += N_hosp
+            N_symp_tot += N_symp
+            hospitalised_fraction_weighted += hospitalised_fraction[idx] * N_symp
+
+        mean   = N_hosp_tot / N_symp_tot
+        sd     = sqrt( mean * ( 1 - mean ) )
+        hospitalised_fraction_weighted  = hospitalised_fraction_weighted / N_symp_tot
+        np.testing.assert_allclose( mean, hospitalised_fraction_weighted, atol = std_error_limit * sd / sqrt( N_symp_tot ) )
+          
+        
+        
        
