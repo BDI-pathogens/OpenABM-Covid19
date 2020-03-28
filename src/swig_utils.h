21af2dc15370f4c9855834c8a878fb0286536348
Daniel Montero <daniel.montero@ibm.com>
2020-03-28 23:14:26 +0000

    Add Python-C interface using Swig

---
+++ src/swig_utils.h
@@ -0,0 +1,7 @@
+#include "model.h"
+
+
+int util_n_current( model *model, int type );
+int util_n_total( model *model, int type );
+int util_n_total_age( model *model, int type, int age );
+int util_n_daily( model *model, int type, int day );
