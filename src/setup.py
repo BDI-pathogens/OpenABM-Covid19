f1b7c8456dc751102e2f031f268c91109d3d31cc
Daniel Montero <daniel.montero@ibm.com>
2020-03-28 23:14:26 +0000

    Add Swig files

---
+++ src/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+setup.py file for SWIG example
+"""
+
+from distutils.core import setup, Extension
+
+
+covid19_module = Extension('_covid19',
+                           sources=['covid19_wrap.c',
+                                    'constant.c',
+                                    'demographics.c',
+                                    'disease.c',
+                                    'individual.c',
+                                    'input.c',
+                                    'interventions.c',
+                                    'model.c',
+                                    'network.c',
+                                    'params.c',
+                                    'utilities.c'
+                                    ],
+                           extra_compile_args=['-g', '-Wall', '-fmessage-length=0', '-I$(INC);', '-O0'],
+                           extra_link_args=['-lgsl', '-lgslcblas', '-lm', '-O3']
+                          )
+
+setup (name = 'covid19',
+       version = '0.1',
+       author      = "SWIG Docs",
+       description = """Simple swig example from docs""",
+       ext_modules = [covid19_module],
+       py_modules = ["covid19"],
+      )
