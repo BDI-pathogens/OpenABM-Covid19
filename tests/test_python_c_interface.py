6a0fb02850ce905c22da2dcf07abbf6e4d7bbebd
p-robot <will@probert.co.nz>
2020-04-04 21:49:10 +0100

    TESTS: Refactored tests to use a single setup and teardown function across all tests called, using pytest fixtures and defined in conftest.py; setup/teardown for each function/method also moved from each test_* file to be defined in conftest.py; removed test_file_exists() method from test_demographics() test_infection_dynamics() and test_network() as it is a mock test; adjusted import scripts on all tests to be consistent; removed constants defined in each test script (directory definitions etc) and imported constant.py in each test script to access these constants

--- tests/test_python_c_interface.py
+++ tests/test_python_c_interface.py
@@ -12,71 +12,27 @@ Created: March 2020
 Author: Daniel Montero
 """
 
-import sys
-import subprocess, shutil, os
+import subprocess, shutil, os, sys, pytest
 from os.path import join
+from string import Template
 import numpy as np, pandas as pd
 from random import randrange
-import pytest
 
 sys.path.append("src/COVID19")
+from parameters import ParameterSet
 from model import Model, Parameters, ModelParamaterException
+
+from . import constant
+from . import utilities as utils
 
 # STEPS > 0
 STEPS = randrange(1, 10)
-PARAM_LINE_NUMBER = 1
-
-# Directories
-IBM_DIR = "src"
-IBM_DIR_TEST = "src_test"
-DATA_DIR_TEST = "data_test"
-
-TEST_DATA_TEMPLATE = "./tests/data/baseline_parameters.csv"
-TEST_DATA_HOUSEHOLD_DEMOGRAPHICS = "./tests/data/baseline_household_demographics.csv"
-TEST_DATA_FILE = join(DATA_DIR_TEST, "test_params.csv")
 
 
 class TestClass(object):
     """
     Test class for checking
     """
-
-    @classmethod
-    def setup_class(self):
-        """
-        When the class is instatiated: compile the IBM in a temporary directory
-        """
-
-        # Make a temporary copy of the code (remove this temporary directory if
-        # it already exists)
-        shutil.rmtree(IBM_DIR_TEST, ignore_errors=True)
-        shutil.copytree(IBM_DIR, IBM_DIR_TEST)
-
-        # Construct the compilation command and compile
-        compile_command = "make clean; make swig-all"
-        completed_compilation = subprocess.run(
-            [compile_command], shell=True, cwd=IBM_DIR_TEST, capture_output=True
-        )
-
-    @classmethod
-    def teardown_class(self):
-        """
-        Remove the temporary code directory (when this class is removed)
-        """
-        shutil.rmtree(IBM_DIR_TEST, ignore_errors=True)
-
-    def setup_method(self):
-        """
-
-        """
-        os.mkdir(DATA_DIR_TEST)
-        shutil.copy(TEST_DATA_TEMPLATE, TEST_DATA_FILE)
-
-    def teardown_method(self):
-        """
-
-        """
-        shutil.rmtree(DATA_DIR_TEST, ignore_errors=True)
 
     """
     def test_lockdown_on_off(self):
@@ -130,10 +86,10 @@ class TestClass(object):
         """
         # Create model object
         params = Parameters(
-            TEST_DATA_TEMPLATE,
-            PARAM_LINE_NUMBER,
-            DATA_DIR_TEST,
-            TEST_DATA_HOUSEHOLD_DEMOGRAPHICS,
+            constant.TEST_DATA_TEMPLATE,
+            constant.PARAM_LINE_NUMBER,
+            constant.DATA_DIR_TEST,
+            constant.TEST_HOUSEHOLD_FILE,
         )
         params.set_param( "app_users_fraction", 0.25)
         model = Model(params)
