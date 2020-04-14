2b34c74a01d9d59b3946c45b5518b3e100b70141
feldnerd <56836977+feldnerd@users.noreply.github.com>
2020-04-14 14:21:11 +0100

    Tidying draft test file

--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -29,22 +29,56 @@ EXECUTABLE = "/Users/feldnerd/Documents/GitHub/COVID19-IBM/src/covid19ibm.exe"
 EXE = f"{EXECUTABLE} {TEST_DATA_FILE} {PARAM_LINE_NUMBER} "+\
     f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {TEST_HOSPITAL_FILE}"
 
-print(EXE)
+# Call the model using baseline parameters, pipe output to file, read output file
+file_output = open(TEST_OUTPUT_FILE, "w")
+completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
+df_output = pd.read_csv(TEST_OUTPUT_FILE, comment = "#", sep = ",")
+
+
+# Create a dataframe out of the terminal output
+numHeader = 10
+numFooter = 27
+df_output = pd.read_csv(TEST_OUTPUT_FILE, 
+    comment = "#", 
+    sep = ",", 
+    skiprows=numHeader, 
+    skipfooter=numFooter, 
+    engine='python')
+
+
+# Write df_output to file
+df_output.to_csv(TEST_OUTPUT_FILE_HOSPITAL, index = False)
 
 class TestClass(object):
     """
     Test class for checking
     """
 
-    def test_hospital(self):
+    def test_hospital_output(self):
         """
-        Test that all columns are non-negative
+        Test that a dataframe of the correct size is being created
         """
-        
-        # Call the model using baseline parameters, pipe output to file, read output file
-        file_output = open(TEST_OUTPUT_FILE, "w")
-        completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
-        df_output = pd.read_csv(TEST_OUTPUT_FILE, comment = "#", sep = ",")
         
         # Assert output not empty
         assert df_output.size > 1
+
+        # output correct size (== time limit)
+        r = df_output.shape[0]
+        assert r == 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
