43d371276d4de2ff56f5f0f5a6cc19f0a29fb55e
Bryn Mathias <bryn.mathias@faculty.ai>
2020-04-16 17:28:39 +0100

    more remove black changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,33 +26,30 @@ def pytest_collection_modifyitems(config, items):
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
 
-
 # "Session" (all files in folder) setup/teardown
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope = "session", autouse = True)
 def compile_covid_ibm(request):
     """
     Compile the IBM in a temporary directory
     """
-    # Make a temporary copy of the code
+    # Make a temporary copy of the code 
     # (remove this temporary directory if it already exists)
-    shutil.rmtree(constant.IBM_DIR_TEST, ignore_errors=True)
+    shutil.rmtree(constant.IBM_DIR_TEST, ignore_errors = True)
     shutil.copytree(constant.IBM_DIR, constant.IBM_DIR_TEST)
 
     # Construct the compilation command and compile
     compile_command = "make clean; make all; make swig-all"
     completed_compilation = subprocess.run(
-        [compile_command], shell=True, cwd=constant.IBM_DIR_TEST, capture_output=True
+        [compile_command], shell = True, cwd = constant.IBM_DIR_TEST, capture_output = True
     )
-
     def fin():
         # Teardown: remove the temporary code directory (when this class is removed)
-        shutil.rmtree(constant.IBM_DIR_TEST, ignore_errors=True)
-
+        shutil.rmtree(constant.IBM_DIR_TEST, ignore_errors = True)
     request.addfinalizer(fin)
 
 
 # Method ("function") setup/teardown
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope = "function", autouse = True)
 def setup_covid_methods(request):
     """
     Called before each method is run; creates a new data dir, copies test datasets
@@ -66,11 +63,9 @@ def setup_covid_methods(request):
     params.set_param("n_total", 10000)
     params.set_param("end_time", 100)
     params.write_params(constant.TEST_DATA_FILE)
-
     def fin():
         """
         At the end of each method (test), remove the directory of test input/output data
         """
         shutil.rmtree(constant.DATA_DIR_TEST, ignore_errors=True)
-
     request.addfinalizer(fin)
