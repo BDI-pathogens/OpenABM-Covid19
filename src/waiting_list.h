497ee57ccde2ea299eeb2bce659deb32c49ed9ba
kelvin-van-vuuren-crick <kelvin.van-vuuren@crick.ac.uk>
2020-04-16 21:43:47 +0100

    added waiting_list struct and updated transition_to_hospitalised func

---
+++ src/waiting_list.h
@@ -0,0 +1,36 @@
+/*
+ * waiting_list.h
+ *
+ *  Created on: 16 Apr 2020
+ *      Author: vuurenk
+ */
+
+#ifndef WAITING_LIST_H_
+#define WAITING_LIST_H_
+
+typedef struct node node;
+
+struct node 
+{
+    long pdx;
+    struct node *next;
+};
+
+typedef struct waiting_list waiting_list;
+
+struct waiting_list 
+{
+    node* head;
+    int size;
+};
+
+node* initialise_node( long pdx );
+void initialise_waiting_list( waiting_list *waiting_list );
+// waiting_list* initialise_waiting_list();
+
+void push( long pdx, waiting_list *waiting_list );
+long pop( waiting_list* waiting_list );
+
+void destroy_waiting_list( waiting_list* waiting_list );
+
+#endif
