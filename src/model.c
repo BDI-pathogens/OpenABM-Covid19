5c070254bd9bf30099eb18d4f8bb1a86b285c097
meadt <xBbTE%84n1r*>
2020-04-21 17:47:26 +0100

    Commit before merge.

--- src/model.c
+++ src/model.c
@@ -772,9 +772,9 @@ int one_time_step( model *model )
     build_daily_network( model );
 	transmit_virus( model, model->params );
 
-	transition_events( model, SYMPTOMATIC,       	   &transition_to_symptomatic,      		FALSE );
-	transition_events( model, SYMPTOMATIC_MILD,  	   &transition_to_symptomatic_mild, 		FALSE );
-	transition_events( model, RECOVERED,         	   &transition_to_recovered,        		FALSE );
+	transition_events( model, SYMPTOMATIC,       	       &transition_to_symptomatic,      		FALSE );
+	transition_events( model, SYMPTOMATIC_MILD,  	       &transition_to_symptomatic_mild, 		FALSE );
+	transition_events( model, RECOVERED,         	       &transition_to_recovered,        		FALSE );
 	transition_events( model, DEATH,             	   &transition_to_death,            		FALSE );
 	transition_events( model, MORTUARY,        		   &transition_to_mortuary,   				FALSE );
     transition_events( model, DISCHARGED,      		   &transition_to_discharged, 				FALSE );
@@ -782,9 +782,9 @@ int one_time_step( model *model )
 	transition_events( model, CRITICAL,          	   &transition_to_critical,         		FALSE );
 	transition_events( model, HOSPITALISED_RECOVERING, &transition_to_hospitalised_recovering,  FALSE );
 
-
 	hospital_waiting_list_transition_scheduler( model, HOSPITALISED );
 	hospital_waiting_list_transition_scheduler( model, CRITICAL );
+
 	transition_events( model, WAITING,         &transition_to_waiting,    FALSE );
     transition_events( model, GENERAL,         &transition_to_general,    FALSE );
     transition_events( model, ICU,             &transition_to_icu,        FALSE );
