39e9dfc0e67537c9c836b1dc7fc0178c97cc9d0f
feldnerd <56836977+feldnerd@users.noreply.github.com>
2020-04-27 14:29:37 +0100

    Updated headers for printf's in main loop

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
 	printf("# rng_seed: %li\n", params.rng_seed);
 	printf("# param_line_number: %d\n", params.param_line_number);
 	
-	printf( "time,lockdown,lockdown_elderly,intervention_on,test_on_symptoms,app_on,total_infected,total_case,n_presymptom,n_asymptom,n_quarantine,n_tests,n_symptoms,n_hospital,n_critical,n_hospitalised_recovering,n_death,n_recovered\n");
+	printf( "time,lockdown,lockdown_elderly,intervention_on,test_on_symptoms,app_on,total_infected,total_case,n_presymptom,n_asymptom,n_quarantine,n_tests,n_symptoms,n_hospital,n_critical,n_hospitalised_recovering,n_death,n_recovered, n_waiting, n_general, n_ICU, n_discharged, n_mortuary\n");
 	last_test = 0;
 	while( model->time < params.end_time && one_time_step( model ) )
 	{
