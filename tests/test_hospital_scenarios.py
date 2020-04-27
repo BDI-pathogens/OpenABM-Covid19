1ab2cf5ccd8cef991785d4175ad7169d51f56ebb
feldnerd <56836977+feldnerd@users.noreply.github.com>
2020-04-27 12:53:01 +0100

    Removed hcw_output file, as currently no tests use it

--- tests/test_hospital_scenarios.py
+++ tests/test_hospital_scenarios.py
@@ -14,13 +14,7 @@ import subprocess, pytest, os, sys
 import numpy as np, pandas as pd
 
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
-TEST_HOSPITAL_FILE = TEST_DIR +"/data/new_hospital_baseline_parameters.csv"
-PYTHON_C_DIR = TEST_DIR.replace("tests","") + "src/COVID19"
-
-SCENARIO_HOSPITAL_FILE = TEST_DIR + "/data/scenario_hospital_baseline_parameters.csv"
-SCENARIO_FILE = TEST_DIR + "/data/scenario_baseline_parameters.csv"
-SCENARIO_ZERO_INFECTIVITY_OUTPUT = TEST_DIR + "/data/scenario_zero_infectivity.csv"
-
+TEST_HOSPITAL_FILE = TEST_DIR +"/data/hospital_baseline_parameters.csv"
 TEST_DATA_FILE = TEST_DIR + "/data/baseline_parameters.csv"
 PARAM_LINE_NUMBER = 1
 DATA_DIR_TEST = TEST_DIR + "/data"
@@ -30,13 +24,18 @@ TEST_OUTPUT_FILE_HOSPITAL = TEST_DIR +"/data/test_hospital_output.csv"
 TEST_OUTPUT_FILE_HOSPITAL_TIME_STEP = TEST_DIR +"/data/time_step_hospital_output.csv"
 TEST_INTERACTIONS_FILE = TEST_DIR +"/data/interactions_Run1.csv"
 TEST_INDIVIDUAL_FILE = TEST_DIR +"/data/individual_file_Run1.csv"
-
 TEST_HCW_FILE = TEST_DIR +"/data/ward_output.csv"
 SRC_DIR = TEST_DIR.replace("tests","") + "src"
 EXECUTABLE = SRC_DIR + "/covid19ibm.exe"
 
+# Use parameter file from Python C interface to adjust parameters
+PYTHON_C_DIR = TEST_DIR.replace("tests","") + "src/COVID19"
 sys.path.append(PYTHON_C_DIR)
 from parameters import ParameterSet
+
+# Files with adjusted parameters for each scenario
+SCENARIO_FILE = TEST_DIR + "/data/scenario_baseline_parameters.csv"
+SCENARIO_HOSPITAL_FILE = TEST_DIR + "/data/scenario_hospital_baseline_parameters.csv"
 
 
 class TestClass(object):
@@ -56,14 +55,14 @@ class TestClass(object):
 
         # Construct the executable command
         EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} "+\
-            f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
+            f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {TEST_HOSPITAL_FILE}"
 
         # Call the model using baseline parameters, pipe output to file, read output file
-        file_output = open(TEST_OUTPUT_FILE, "r")
+        file_output = open(TEST_OUTPUT_FILE, "w")
         completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
-        df_individual_output = pd.read_csv(TEST_INDIVIDUAL_FILE)
 
         # In the individual file, time_infected should be not equal to -1 in n_seed_infection number of cases
+        df_individual_output = pd.read_csv(TEST_INDIVIDUAL_FILE)
         expected_output = int(params.get_param("n_seed_infection"))
         output = df_individual_output["time_infected"] != -1
         output = df_individual_output[output]
@@ -74,7 +73,7 @@ class TestClass(object):
 
     def test_zero_space(self):
         """
-        Set hospital infection modifier to zero and also hospital space to zero
+        Set hospital space to zero
         """
 
         # Adjust hospital baseline parameter
@@ -84,20 +83,13 @@ class TestClass(object):
         h_params.write_params(SCENARIO_HOSPITAL_FILE)
 
         # Construct the executable command
-        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} "+\
+        EXE = f"{EXECUTABLE} {TEST_DATA_FILE} {PARAM_LINE_NUMBER} "+\
             f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
-
-        # Construct the compilation command and compile
-        compile_command = "make clean; make all"
-        completed_compilation = subprocess.run([compile_command], 
-            shell = True, 
-            cwd = SRC_DIR, 
-            capture_output = True
-            )
 
         # Call the model using baseline parameters, pipe output to file, read output file
         file_output = open(TEST_OUTPUT_FILE, "w")
         completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
+
         df_individual_output = pd.read_csv(TEST_INDIVIDUAL_FILE)
         n_hospitalised = df_individual_output["time_hospitalised"] != -1
         n_hospitalised = df_individual_output[n_hospitalised]
