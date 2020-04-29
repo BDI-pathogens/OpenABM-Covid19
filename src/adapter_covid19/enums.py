39b7b534087388f6c6bd0205348992ec94c64d34
Feifan Chen <feifan.chen@gs.com>
2020-04-29 22:04:44 +0000

    add decile as an enum

--- src/adapter_covid19/enums.py
+++ src/adapter_covid19/enums.py
@@ -159,3 +159,15 @@ class M(OrderedEnum):
     I = "imports"
     L = "labour"
     K = "capital"
+
+
+class Decile(OrderedEnum):
+    one = "1"
+    two = "2"
+    three = "3"
+    four = "4"
+    five = "5"
+    six = "6"
+    seven = "7"
+    eight = "8"
+    nine = "9"
