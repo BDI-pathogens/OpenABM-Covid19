39008bf71cd6d47ad05c25533fd54edec6888786
kelvin-van-vuuren-crick <kelvin.van-vuuren@crick.ac.uk>
2020-05-05 14:27:09 +0100

    added test_hospital_infectivity_modifiers to check no hcw infected by patients when infectivity modifiers set to zero

--- tests/test_hospital_scenarios.py
+++ tests/test_hospital_scenarios.py
@@ -14,22 +14,22 @@ import subprocess, pytest, os, sys
 import numpy as np, pandas as pd
 
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
-TEST_HOSPITAL_FILE = TEST_DIR +"/data/hospital_baseline_parameters.csv"
+TEST_HOSPITAL_FILE = TEST_DIR + "/data/hospital_baseline_parameters.csv"
 TEST_DATA_FILE = TEST_DIR + "/data/baseline_parameters.csv"
 PARAM_LINE_NUMBER = 1
 DATA_DIR_TEST = TEST_DIR + "/data"
 TEST_HOUSEHOLD_FILE = TEST_DIR + "/data/baseline_household_demographics.csv"
-TEST_OUTPUT_FILE = TEST_DIR +"/data/test_output.csv"
-TEST_OUTPUT_FILE_HOSPITAL = TEST_DIR +"/data/test_hospital_output.csv"
-TEST_OUTPUT_FILE_HOSPITAL_TIME_STEP = TEST_DIR +"/data/time_step_hospital_output.csv"
-TEST_INTERACTIONS_FILE = TEST_DIR +"/data/interactions_Run1.csv"
-TEST_INDIVIDUAL_FILE = TEST_DIR +"/data/individual_file_Run1.csv"
-TEST_HCW_FILE = TEST_DIR +"/data/ward_output.csv"
-SRC_DIR = TEST_DIR.replace("tests","") + "src"
+TEST_OUTPUT_FILE = TEST_DIR + "/data/test_output.csv"
+TEST_OUTPUT_FILE_HOSPITAL = TEST_DIR + "/data/test_hospital_output.csv"
+TEST_OUTPUT_FILE_HOSPITAL_TIME_STEP = TEST_DIR + "/data/time_step_hospital_output.csv"
+TEST_INTERACTIONS_FILE = TEST_DIR + "/data/interactions_Run1.csv"
+TEST_INDIVIDUAL_FILE = TEST_DIR + "/data/individual_file_Run1.csv"
+TEST_HCW_FILE = TEST_DIR + "/data/ward_output.csv"
+SRC_DIR = TEST_DIR.replace("tests", "") + "src"
 EXECUTABLE = SRC_DIR + "/covid19ibm.exe"
 
 # Use parameter file from Python C interface to adjust parameters
-PYTHON_C_DIR = TEST_DIR.replace("tests","") + "src/COVID19"
+PYTHON_C_DIR = TEST_DIR.replace("tests", "") + "src/COVID19"
 sys.path.append(PYTHON_C_DIR)
 from parameters import ParameterSet
 from . import constant
@@ -58,19 +58,19 @@ class TestClass(object):
         # Construct the compilation command and compile
         # compile_command = "make clean; make all"
         compile_command = "make clean; make all; make swig-all;"
-        completed_compilation = subprocess.run([compile_command], 
-            shell = True, 
-            cwd = SRC_DIR, 
-            capture_output = True
-            )
+        completed_compilation = subprocess.run([compile_command],
+                                               shell=True,
+                                               cwd=SRC_DIR,
+                                               capture_output=True
+                                               )
 
         # Construct the executable command
-        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} "+\
-            f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {TEST_HOSPITAL_FILE}"
+        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} " + \
+              f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {TEST_HOSPITAL_FILE}"
 
         # Call the model using baseline parameters, pipe output to file, read output file
         file_output = open(TEST_OUTPUT_FILE, "w")
-        completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
+        completed_run = subprocess.run([EXE], stdout=file_output, shell=True)
         df_output = pd.read_csv(TEST_OUTPUT_FILE, comment="#", sep=",")
 
         # Check that the simulation ran
@@ -85,7 +85,6 @@ class TestClass(object):
         output = len(output.index)
 
         np.testing.assert_equal(output, expected_output)
-
 
     def test_zero_beds(self):
         """
@@ -106,18 +105,18 @@ class TestClass(object):
         # Construct the compilation command and compile
         compile_command = "make clean; make all; make swig-all;"
         completed_compilation = subprocess.run([compile_command],
-            shell = True,
-            cwd = SRC_DIR,
-            capture_output = True
-            )
+                                               shell=True,
+                                               cwd=SRC_DIR,
+                                               capture_output=True
+                                               )
 
         # Construct the executable command
-        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} "+\
-            f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
+        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} " + \
+              f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
 
         # Call the model pipe output to file, read output file
         file_output = open(TEST_OUTPUT_FILE, "w")
-        completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
+        completed_run = subprocess.run([EXE], stdout=file_output, shell=True)
         df_output = pd.read_csv(TEST_OUTPUT_FILE, comment="#", sep=",")
 
         # Check that the simulation ran
@@ -151,19 +150,19 @@ class TestClass(object):
 
         # Construct the compilation command and compile
         compile_command = "make clean; make all; make swig-all;"
-        completed_compilation = subprocess.run([compile_command], 
-            shell = True, 
-            cwd = SRC_DIR, 
-            capture_output = True
-            )
+        completed_compilation = subprocess.run([compile_command],
+                                               shell=True,
+                                               cwd=SRC_DIR,
+                                               capture_output=True
+                                               )
 
         # Construct the executable command
-        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} "+\
-            f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
+        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} " + \
+              f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
 
         # Call the model pipe output to file, read output file
         file_output = open(TEST_OUTPUT_FILE, "w")
-        completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
+        completed_run = subprocess.run([EXE], stdout=file_output, shell=True)
         df_output = pd.read_csv(TEST_OUTPUT_FILE, comment="#", sep=",")
 
         # Check that the simulation ran
@@ -176,7 +175,6 @@ class TestClass(object):
         n_patient_general = len(n_patient_general.index)
 
         assert n_patient_general == 0
-
 
     def test_zero_icu_wards(self):
         """
@@ -195,19 +193,19 @@ class TestClass(object):
 
         # Construct the compilation command and compile
         compile_command = "make clean; make all; make swig-all;"
-        completed_compilation = subprocess.run([compile_command], 
-            shell = True, 
-            cwd = SRC_DIR, 
-            capture_output = True
-            )
+        completed_compilation = subprocess.run([compile_command],
+                                               shell=True,
+                                               cwd=SRC_DIR,
+                                               capture_output=True
+                                               )
 
         # Construct the executable command
-        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} "+\
-            f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
+        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} " + \
+              f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
 
         # Call the model pipe output to file, read output file
         file_output = open(TEST_OUTPUT_FILE, "w")
-        completed_run = subprocess.run([EXE], stdout = file_output, shell = True)
+        completed_run = subprocess.run([EXE], stdout=file_output, shell=True)
         df_output = pd.read_csv(TEST_OUTPUT_FILE, comment="#", sep=",")
 
         # Check that the simulation ran
@@ -219,7 +217,6 @@ class TestClass(object):
         n_patient_icu = df_individual_output[n_patient_icu]
         n_patient_icu = len(n_patient_icu.index)
         assert n_patient_icu == 0
-        
 
     def test_zero_hcw_patient_interactions(self):
         """
@@ -253,12 +250,16 @@ class TestClass(object):
         file_output = open(TEST_OUTPUT_FILE, "w")
         completed_run = subprocess.run([EXE], stdout=file_output, shell=True)
         df_interactions = pd.read_csv(TEST_INTERACTIONS_FILE,
-                             comment="#", sep=",", skipinitialspace=True)
+                                      comment="#", sep=",", skipinitialspace=True)
 
-        df_doctor_patient_general_interactions = df_interactions[df_interactions["type"] == constant.HOSPITAL_DOCTOR_PATIENT_GENERAL]
-        df_nurse_patient_general_interactions  = df_interactions[df_interactions["type"] == constant.HOSPITAL_NURSE_PATIENT_GENERAL]
-        df_doctor_patient_icu_interactions = df_interactions[df_interactions["type"] == constant.HOSPITAL_DOCTOR_PATIENT_ICU]
-        df_nurse_patient_icu_interactions  = df_interactions[df_interactions["type"] == constant.HOSPITAL_NURSE_PATIENT_ICU]
+        df_doctor_patient_general_interactions = df_interactions[
+            df_interactions["type"] == constant.HOSPITAL_DOCTOR_PATIENT_GENERAL]
+        df_nurse_patient_general_interactions = df_interactions[
+            df_interactions["type"] == constant.HOSPITAL_NURSE_PATIENT_GENERAL]
+        df_doctor_patient_icu_interactions = df_interactions[
+            df_interactions["type"] == constant.HOSPITAL_DOCTOR_PATIENT_ICU]
+        df_nurse_patient_icu_interactions = df_interactions[
+            df_interactions["type"] == constant.HOSPITAL_NURSE_PATIENT_ICU]
 
         assert len(df_doctor_patient_general_interactions) == 0
         assert len(df_nurse_patient_general_interactions) == 0
@@ -308,3 +309,49 @@ class TestClass(object):
         n_infected = df_individual_output["time_infected"] != -1
         n_recovered = df_individual_output["time_recovered"] != -1
         assert len(df_individual_output[n_infected].index) == len(df_individual_output[n_recovered].index)
+
+    def test_hospital_infectivity_modifiers(self):
+        """
+        Set patient infectivity modifiers to zero and test that no healthcare
+        workers are infected by a patient
+        """
+        params = ParameterSet(TEST_DATA_FILE, line_number=1)
+        params.set_param("n_total", 20000)
+        params.write_params(SCENARIO_FILE)
+        # Adjust hospital baseline parameter
+        h_params = ParameterSet(TEST_HOSPITAL_FILE, line_number=1)
+        h_params.set_param("waiting_infectivity_modifier", 0)
+        h_params.set_param("general_infectivity_modifier", 0)
+        h_params.set_param("icu_infectivity_modifier", 0)
+        h_params.write_params(SCENARIO_HOSPITAL_FILE)
+        # Construct the compilation command and compile
+        compile_command = "make clean; make all; make swig-all;"
+        completed_compilation = subprocess.run([compile_command],
+                                               shell=True,
+                                               cwd=SRC_DIR,
+                                               capture_output=True
+                                               )
+
+        # Construct the executable command
+        EXE = f"{EXECUTABLE} {SCENARIO_FILE} {PARAM_LINE_NUMBER} " + \
+              f"{DATA_DIR_TEST} {TEST_HOUSEHOLD_FILE} {SCENARIO_HOSPITAL_FILE}"
+
+        # Call the model pipe output to file, read output file
+        file_output = open(TEST_OUTPUT_FILE, "w")
+        completed_run = subprocess.run([EXE], stdout=file_output, shell=True)
+        df_output = pd.read_csv(TEST_OUTPUT_FILE, comment="#", sep=",")
+
+        # Check that the simulation ran
+        assert len(df_output) != 0
+
+        df_individual_output = pd.read_csv(TEST_INDIVIDUAL_FILE)
+        # get healthcare workers
+        healthcare_workers = df_individual_output["worker_type"] != constant.NOT_HEALTHCARE_WORKER
+        healthcare_workers = df_individual_output[healthcare_workers]
+
+        # check that no healthcare workers have been infected by a patient
+        for healthcare_worker in healthcare_workers:
+            infector_hospital_state = healthcare_worker["infector_hospital_state"]
+            assert infector_hospital_state != constant.EVENT_TYPES.WAITING
+            assert infector_hospital_state != constant.EVENT_TYPES.GENERAL
+            assert infector_hospital_state != constant.EVENT_TYPES.ICU
