d01c75eb963c3851b735e4d4606989c678a3b088
Arun Lobo <arun.lobo@gs.com>
2020-05-06 07:45:09 +0100

    Fix tests

--- tests/adapter_covid19/test_gdp_models.py
+++ tests/adapter_covid19/test_gdp_models.py
@@ -4,12 +4,18 @@ Basic example testing the adaptER-covid19 GDP models
 
 import sys
 
+from adapter_covid19.data_structures import SimulateState
 from adapter_covid19.datasources import Reader
-from adapter_covid19.gdp import LinearGdpModel, SupplyDemandGdpModel
+from adapter_covid19.gdp import (
+    LinearGdpModel,
+    SupplyDemandGdpModel,
+    PiecewiseLinearCobbDouglasGdpModel,
+)
 from tests.adapter_covid19.utilities import (
     MAX_UTILISATIONS,
     MIN_UTILISATIONS,
     DATA_PATH,
+    FLAT_UTILISATIONS,
 )
 
 sys.path.append("src/adapter_covid19")
@@ -17,19 +23,22 @@ sys.path.append("src/adapter_covid19")
 
 def pytest_generate_tests(metafunc):
     if "gdp_model_cls" in metafunc.fixturenames:
-        metafunc.parametrize("gdp_model_cls", [LinearGdpModel, SupplyDemandGdpModel])
+        metafunc.parametrize(
+            "gdp_model_cls",
+            [LinearGdpModel, SupplyDemandGdpModel, PiecewiseLinearCobbDouglasGdpModel],
+        )
     if "utilisations" in metafunc.fixturenames:
-        metafunc.parametrize("utilisations", [MAX_UTILISATIONS, MIN_UTILISATIONS])
-    if "lockdown" in metafunc.fixturenames:
-        metafunc.parametrize("lockdown", [True, False])
+        metafunc.parametrize(
+            "utilisations", [MAX_UTILISATIONS, MIN_UTILISATIONS, FLAT_UTILISATIONS]
+        )
 
 
 class TestClass:
-    def test_interface(self, gdp_model_cls, lockdown, utilisations):
+    def test_interface(self, gdp_model_cls, utilisations):
         reader = Reader(DATA_PATH)
+        state = SimulateState(time=0, lockdown=False, utilisations=utilisations)
         gdp_model = gdp_model_cls()
         gdp_model.load(reader)
-        gdp_model.simulate(
-            time=0, lockdown=lockdown, lockdown_exit_time=0, utilisations=utilisations
-        )
-        assert 0 <= sum(gdp_model.results.gdp[0].values()) <= gdp_model.results.max_gdp
+        gdp_model.simulate(state)
+        # Factor of 1.1 is because of the GDP backbone model
+        assert 0 <= sum(state.gdp_state.gdp.values()) <= state.gdp_state.max_gdp * 1.1
--- tests/adapter_covid19/test_personal_bankruptcy_models.py
+++ tests/adapter_covid19/test_personal_bankruptcy_models.py
@@ -4,6 +4,7 @@ Basic example testing the adaptER-covid19 personal bankruptcy models
 
 import sys
 
+from adapter_covid19.data_structures import SimulateState
 from adapter_covid19.datasources import Reader
 from adapter_covid19.enums import Region
 from adapter_covid19.personal_insolvency import PersonalBankruptcyModel
@@ -11,6 +12,7 @@ from tests.adapter_covid19.utilities import (
     DATA_PATH,
     MIN_UTILISATIONS,
     MAX_UTILISATIONS,
+    FLAT_UTILISATIONS,
 )
 
 sys.path.append("src/adapter_covid19")
@@ -20,16 +22,17 @@ def pytest_generate_tests(metafunc):
     if "personal_bankruptcy_model_cls" in metafunc.fixturenames:
         metafunc.parametrize("personal_bankruptcy_model_cls", [PersonalBankruptcyModel])
     if "utilisations" in metafunc.fixturenames:
-        metafunc.parametrize("utilisations", [MAX_UTILISATIONS, MIN_UTILISATIONS])
+        metafunc.parametrize(
+            "utilisations", [MAX_UTILISATIONS, MIN_UTILISATIONS, FLAT_UTILISATIONS]
+        )
 
 
 class TestClass:
     def test_interface(self, personal_bankruptcy_model_cls, utilisations):
         reader = Reader(DATA_PATH)
+        state = SimulateState(time=0, lockdown=False, utilisations=utilisations)
         pb_model = personal_bankruptcy_model_cls()
         pb_model.load(reader)
-        pb_model.simulate(
-            time=0, utilisations=utilisations,
-        )
+        pb_model.simulate(state)
         for region in Region:
-            assert 0 <= pb_model.results[0][region].personal_bankruptcy <= 1
+            assert 0 <= state.personal_state.personal_bankruptcy[region] <= 1
