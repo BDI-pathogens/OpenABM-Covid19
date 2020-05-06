ecd11dcd19b672761ab52c91ea81195ee3614e37
Arun Lobo <arun.lobo@gs.com>
2020-05-06 01:43:10 +0100

    Fix personal bankruptcy tests

--- tests/adapter_covid19/test_personal_bankruptcy_models.py
+++ tests/adapter_covid19/test_personal_bankruptcy_models.py
@@ -4,14 +4,13 @@ Basic example testing the adaptER-covid19 personal bankruptcy models
 
 import sys
 
-from adapter_covid19.enums import Region
-
 from adapter_covid19.datasources import Reader
+from adapter_covid19.enums import Region
 from adapter_covid19.personal_insolvency import PersonalBankruptcyModel
 from tests.adapter_covid19.utilities import (
     DATA_PATH,
-    MAX_SECTOR_UTILISATIONS,
-    MIN_SECTOR_UTILISATIONS,
+    MIN_UTILISATIONS,
+    MAX_UTILISATIONS,
 )
 
 sys.path.append("src/adapter_covid19")
@@ -20,23 +19,17 @@ sys.path.append("src/adapter_covid19")
 def pytest_generate_tests(metafunc):
     if "personal_bankruptcy_model_cls" in metafunc.fixturenames:
         metafunc.parametrize("personal_bankruptcy_model_cls", [PersonalBankruptcyModel])
-    if "gdp_discount" in metafunc.fixturenames:
-        metafunc.parametrize(
-            "gdp_discount", [MAX_SECTOR_UTILISATIONS, MIN_SECTOR_UTILISATIONS]
-        )
-    if "lockdown" in metafunc.fixturenames:
-        metafunc.parametrize("lockdown", [True, False])
+    if "utilisations" in metafunc.fixturenames:
+        metafunc.parametrize("utilisations", [MAX_UTILISATIONS, MIN_UTILISATIONS])
 
 
 class TestClass:
-    def test_interface(self, personal_bankruptcy_model_cls, lockdown, gdp_discount):
+    def test_interface(self, personal_bankruptcy_model_cls, utilisations):
         reader = Reader(DATA_PATH)
         pb_model = personal_bankruptcy_model_cls()
         pb_model.load(reader)
         pb_model.simulate(
-            time=0,
-            lockdown=lockdown,
-            corporate_bankruptcy={k: 1 - v for k, v in gdp_discount.items()},
+            time=0, utilisations=utilisations,
         )
         for region in Region:
             assert 0 <= pb_model.results[0][region].personal_bankruptcy <= 1
--- tests/adapter_covid19/utilities.py
+++ tests/adapter_covid19/utilities.py
@@ -1,9 +1,14 @@
+import copy
 import itertools
 
-from adapter_covid19.enums import Region, Sector, Age
+from adapter_covid19.enums import Region, Sector, Age, LabourState
 
-MAX_UTILISATIONS = {k: 1 for k in itertools.product(Region, Sector, Age)}
-MIN_UTILISATIONS = {k: 0 for k in itertools.product(Region, Sector, Age)}
+BASE_UTILISATIONS = {k: 0 for k in itertools.product(LabourState, Region, Sector, Age)}
+MAX_UTILISATIONS = copy.deepcopy(BASE_UTILISATIONS)
+MIN_UTILISATIONS = copy.deepcopy(BASE_UTILISATIONS)
+for r, s, a in itertools.product(Region, Sector, Age):
+    MAX_UTILISATIONS[LabourState.WORKING, r, s, a] = 1
+    MIN_UTILISATIONS[LabourState.ILL, r, s, a] = 1
 MAX_SECTOR_UTILISATIONS = {k: 1 for k in Sector}
 MIN_SECTOR_UTILISATIONS = {k: 0 for k in Sector}
 
