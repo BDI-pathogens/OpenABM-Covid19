be7247248896c2209cd9fc4925342892c23910fe
Arun Lobo <arun.lobo@gs.com>
2020-05-07 20:36:39 +0100

    Remove deprecated add_employment in Economics
    Now done in GDP mnodel

--- src/adapter_covid19/economics.py
+++ src/adapter_covid19/economics.py
@@ -1,15 +1,11 @@
-import itertools
 import logging
-from typing import Mapping, Tuple
 
-from adapter_covid19.constants import START_OF_TIME
 from adapter_covid19.corporate_bankruptcy import CorporateBankruptcyModel
 from adapter_covid19.data_structures import (
     SimulateState,
     Utilisations,
 )
 from adapter_covid19.datasources import Reader
-from adapter_covid19.enums import Region, Sector, Age, LabourState
 from adapter_covid19.gdp import BaseGdpModel
 from adapter_covid19.personal_insolvency import PersonalBankruptcyModel
 
@@ -55,46 +51,6 @@ class Economics:
         return self._simulate(
             simulate_state, simulate_state.time, simulate_state.utilisations,
         )
-
-    def add_unemployment(
-        self,
-        utilisations: Mapping[Tuple[LabourState, Region, Sector, Age], float],
-        unemployment_per_sector: Mapping[Sector, float],
-    ) -> Mapping[Tuple[LabourState, Region, Sector, Age], float]:
-        # TODO: remove
-        unemployed = {
-            (LabourState.UNEMPLOYED, r, s, a): sum(
-                utilisations[l, r, s, a] * unemployment_per_sector[s]
-                for l in [LabourState.WORKING, LabourState.WFH, LabourState.FURLOUGHED]
-            )
-            for r, s, a in itertools.product(Region, Sector, Age)
-        }
-        working = {
-            (LabourState.WORKING, r, s, a): utilisations[LabourState.WORKING, r, s, a]
-            * (1 - unemployment_per_sector[s])
-            for r, s, a in itertools.product(Region, Sector, Age)
-        }
-        wfh = {
-            (LabourState.WFH, r, s, a): utilisations[LabourState.WFH, r, s, a]
-            * (1 - unemployment_per_sector[s])
-            for r, s, a in itertools.product(Region, Sector, Age)
-        }
-        furloughed = {
-            (LabourState.FURLOUGHED, r, s, a): utilisations[
-                LabourState.FURLOUGHED, r, s, a
-            ]
-            * (1 - unemployment_per_sector[s])
-            for r, s, a in itertools.product(Region, Sector, Age)
-        }
-        new_utilisations = {
-            (LabourState.ILL, r, s, a): utilisations[LabourState.ILL, r, s, a]
-            for r, s, a in itertools.product(Region, Sector, Age)
-        }
-        new_utilisations.update(unemployed)
-        new_utilisations.update(working)
-        new_utilisations.update(wfh)
-        new_utilisations.update(furloughed)
-        return new_utilisations
 
     def _simulate(
         self, state: SimulateState, time: int, utilisations: Utilisations,
