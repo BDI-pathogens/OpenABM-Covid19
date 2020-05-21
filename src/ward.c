4c9d5d0775f34796af5ad7751911fde3d8a2f026
kelvin van vuuren <kelvin.van-vuuren@crick.ac.uk>
2020-05-21 18:19:19 +0100

    adding patient required interaction check to hcw patient network rebuild

--- src/ward.c
+++ src/ward.c
@@ -96,50 +96,53 @@ void build_ward_networks( model *model, ward* ward )
 
 void build_hcw_patient_network( ward* ward, network *network, long *hc_workers, int n_hcw_working, int n_patient_required_interactions, int max_hcw_daily_interactions )
 {
-    int idx, hdx, patient_interactions_per_hcw, n_total_interactions, n_pos;
-    long *all_required_interactions, *capped_hcw_interactions;
-
-    //Determine the number of interactions that all patients need.
-    patient_interactions_per_hcw = round( (n_patient_required_interactions * ward->patients->size) / n_hcw_working );
-    //TODO: should there be different max interactions for doctors / nurses?
-    //Check whether the number of required interactions is greater than the max possible. If it is, set it to the max.
-    patient_interactions_per_hcw = (patient_interactions_per_hcw > max_hcw_daily_interactions) ? max_hcw_daily_interactions : patient_interactions_per_hcw;
-
-    n_total_interactions = patient_interactions_per_hcw * n_hcw_working;
-
-    all_required_interactions = calloc( n_patient_required_interactions * ward->patients->size, sizeof(long) );
-    capped_hcw_interactions   = calloc( n_total_interactions, sizeof(long) );
-
-    network->n_edges = 0;
-    network->n_vertices       = n_hcw_working + ward->patients->size;
-
-    n_pos = 0;
-
-    for( int idx = 0; idx < ward->patients->size; idx++ )
-        for( int i = 0; i < n_patient_required_interactions; i++)
-            all_required_interactions[n_pos++] = list_element_at(ward->patients, idx);
-
-    //shuffle list of all interactions
-    gsl_ran_shuffle( rng, all_required_interactions, n_pos, sizeof(long) );
-
-    idx = 0;
-    hdx = 0;
-    n_total_interactions--;
-    //assign network edges between hcw and randomly picked patient interactions
-    //TODO: shift patterns eg day off
-    while( idx < n_total_interactions )
+    if( n_patient_required_interactions > 0 )
     {
-        network->edges[network->n_edges].id1 = hc_workers[ hdx ];
-        network->edges[network->n_edges].id2 = all_required_interactions[ idx++ ];
+        int idx, hdx, patient_interactions_per_hcw, n_total_interactions, n_pos;
+        long *all_required_interactions, *capped_hcw_interactions;
 
-        network->n_edges++;
-        hdx++;
-        if( hdx >= n_hcw_working )
-            hdx = 0;
+        //Determine the number of interactions that all patients need.
+        patient_interactions_per_hcw = round( (n_patient_required_interactions * ward->patients->size) / n_hcw_working );
+        //TODO: should there be different max interactions for doctors / nurses?
+        //Check whether the number of required interactions is greater than the max possible. If it is, set it to the max.
+        patient_interactions_per_hcw = (patient_interactions_per_hcw > max_hcw_daily_interactions) ? max_hcw_daily_interactions : patient_interactions_per_hcw;
+
+        n_total_interactions = patient_interactions_per_hcw * n_hcw_working;
+
+        all_required_interactions = calloc( n_patient_required_interactions * ward->patients->size, sizeof(long) );
+        capped_hcw_interactions   = calloc( n_total_interactions, sizeof(long) );
+
+        network->n_edges = 0;
+        network->n_vertices       = n_hcw_working + ward->patients->size;
+
+        n_pos = 0;
+
+        for( int idx = 0; idx < ward->patients->size; idx++ )
+            for( int i = 0; i < n_patient_required_interactions; i++)
+                all_required_interactions[n_pos++] = list_element_at(ward->patients, idx);
+
+        //shuffle list of all interactions
+        gsl_ran_shuffle( rng, all_required_interactions, n_pos, sizeof(long) );
+
+        idx = 0;
+        hdx = 0;
+        n_total_interactions--;
+        //assign network edges between hcw and randomly picked patient interactions
+        //TODO: shift patterns eg day off
+        while( idx < n_total_interactions )
+        {
+            network->edges[network->n_edges].id1 = hc_workers[ hdx ];
+            network->edges[network->n_edges].id2 = all_required_interactions[ idx++ ];
+
+            network->n_edges++;
+            hdx++;
+            if( hdx >= n_hcw_working )
+                hdx = 0;
+        }
+
+        free( all_required_interactions );
+        free( capped_hcw_interactions );
     }
-
-    free( all_required_interactions );
-    free( capped_hcw_interactions );
 }
 
 int add_patient_to_ward( ward *ward, long pdx )
