114ad2333d7cde8027dd1c228ac20223e775abfd
meadt <xBbTE%84n1r*>
2020-05-27 14:46:22 +0100

    Added setup.py back into src

---
+++ src/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+"""
+setup.py file for SWIG example
+"""
+
+from distutils.core import setup, Extension
+
+covid19_module = Extension(
+    "_covid19",
+    sources=[
+        "covid19_wrap.c",
+        "constant.c",
+        "demographics.c",
+        "disease.c",
+        "doctor.c",
+        "hospital.c",
+        "individual.c",
+        "input.c",
+        "interventions.c",
+        "list.c",
+        "model.c",
+        "network.c",
+        "nurse.c",
+        "params.c",
+        "utilities.c",
+        "ward.c"
+    ],
+    extra_compile_args=["-g", "-Wall", "-fmessage-length=0", "-I$(INC);", "-O0"],
+    extra_link_args=["-lgsl", "-lgslcblas", "-lm", "-O3"],
+)
+
+setup(
+    name="covid19",
+    version="0.2",
+    author="SWIG Docs",
+    description="""Individual-based model for modelling of a COVID-19 outbreak""",
+    ext_modules=[covid19_module],
+    packages=[
+        'COVID19',
+        'adapter_covid19',
+    ],
+    py_modules=[
+        'covid19',
+    ],
+    install_requires=[
+        "click",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "tqdm",
+    ]
+)
