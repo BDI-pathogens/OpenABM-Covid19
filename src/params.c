f1b6bcfc72d4296035dd64991cf6797f7a91d3dd
roberthinch <robert.hinch@gmail.com>
2020-06-26 11:29:33 +0100

    Prevent recursive tests running when test turn around os 0 days

--- src/params.c
+++ src/params.c
@@ -722,6 +722,7 @@ int set_model_param_test_on_traced( model *model, int value )
 int set_model_param_test_result_wait( model *model, int value )
 {
     model->params->test_result_wait = value;
+	check_params( model->params );
     return TRUE;
 }
 
@@ -732,6 +733,7 @@ int set_model_param_test_result_wait( model *model, int value )
 int set_model_param_test_order_wait( model *model, int value )
 {
     model->params->test_order_wait = value;
+	check_params( model->params );
     return TRUE;
 }
 
@@ -749,6 +751,8 @@ int set_model_param_test_result_wait_priority( model *model, int value )
     if( value == NO_PRIORITY_TEST )
     	model->params->test_order_wait_priority = NO_PRIORITY_TEST;
 
+	check_params( model->params );
+
     return TRUE;
 }
 
@@ -765,6 +769,8 @@ int set_model_param_test_order_wait_priority( model *model, int value )
 
      if( value == NO_PRIORITY_TEST )
      	model->params->test_result_wait_priority = NO_PRIORITY_TEST;
+
+ 	check_params( model->params );
 
     return TRUE;
 }
@@ -1243,6 +1249,18 @@ void check_params( parameters *params )
 		for( idx = 0; idx < N_AGE_TYPES; idx++ )
 			if( params->mean_random_interactions[idx] >= params->sd_random_interactions[idx] * params->sd_random_interactions[idx] )
 				print_exit( "BAD_PARAM - sd_random_interations_xxxx - variance must be greater than the mean for (negative binomial distribution");
+
+    if( params->test_on_traced )
+    {
+    	if( params->test_order_wait + params->test_result_wait == 0 )
+    		print_exit( "BAD_PARAM - total test time must be at least a day if using recursive testing");
+
+    	if( params->test_order_wait_priority != NO_PRIORITY_TEST )
+    	{
+    		if( params->test_order_wait_priority + params->test_result_wait_priority == 0 )
+    			print_exit( "BAD_PARAM - total test time must be at least a day if using recursive testing");
+    	}
+    }
 }
 
 /*****************************************************************************************
