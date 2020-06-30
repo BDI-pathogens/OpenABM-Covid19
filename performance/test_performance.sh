f76aa809e44ad55ea167ec58d6d3d826ab0ffe5b
p-robot <will@probert.co.nz>
2020-06-30 17:49:34 +0100

    Add changes suggested by @RossBoylan for generalising the performance script

--- performance/test_performance.sh
+++ performance/test_performance.sh
@@ -10,13 +10,19 @@ PROFILE=1 # 0=no profile; 1=time; 2=memory
 
 START=`date +%s`
 if [ $PROFILE == 1 ]
-then
-$EXE $PARAM_DIR 1 & PID=$!
-instruments -l 60000 -t Time\ Profiler -p $PID 
+then if (which instruments > /dev/null); then
+	 $EXE $PARAM_DIR 1 & PID=$!
+	 instruments -l 60000 -t Time\ Profiler -p $PID ;
+     else
+	 PROFILE=0;
+     fi
 fi
 if [ $PROFILE == 2 ]
-then
-iprofiler -allocations -T 20s $EXE $PARAM_DIR 1 
+then if (which iprofiler > /dev/null); then
+	 iprofiler -allocations -T 20s $EXE $PARAM_DIR 1;
+     else
+	 PROFILE=0;
+     fi
 fi
 if [ $PROFILE == 0 ]
 then
@@ -27,8 +33,7 @@ fi
 END=`date +%s`
 RUNTIME=$((END-START))
 echo "execution time was" $RUNTIME "seconds"
-if [ $RUNTIME \> 40 ] 
+if (( $RUNTIME > 40 )); 
 then
     printf  "\n*********************************\n** FAILURE - TOO SLOW\n*********************************\n\n"
 fi
-
