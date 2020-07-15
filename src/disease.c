35b9e03bffcf274ea829a243b5d9247374f9f279
Bryn Mathias <bryn.mathias@faculty.ai>
2020-07-15 11:11:53 +0100

    Re enable tracking of `hospital_admissions` and `hospital_to_critical_daily` to model when nosocomial mode is disabled (#147)
    * add a nosocomial flag to the model in python, re-enable the hospital_admissions and hospital_to_critical_daily values in the old way if we're not running in nosocomial mode
    
    * split up tests to one per variable and add a daily deaths count

--- src/disease.c
+++ src/disease.c
@@ -287,7 +287,7 @@ void transition_one_disese_event(
 		remove_event_from_event_list( model, indiv->current_disease_event );
 	if( indiv->next_disease_event != NULL )
 		indiv->current_disease_event = indiv->next_disease_event;
-	
+
 	if( indiv->quarantined == TRUE){
 		if(from == SUSCEPTIBLE){
 			model->n_quarantine_infected++;
@@ -352,6 +352,7 @@ void transition_to_hospitalised( model *model, individual *indiv )
 {
 	set_hospitalised( indiv, model->params, model->time );
 
+
 	if( model->params->hospital_on )
 	{
 		int assigned_hospital_idx = find_least_full_hospital( model, COVID_GENERAL );
@@ -359,6 +360,9 @@ void transition_to_hospitalised( model *model, individual *indiv )
 	}
 	else
 	{
+		model->event_lists[TRANSITION_TO_HOSPITAL].n_daily_current[model->time]+=1;
+		model->event_lists[TRANSITION_TO_HOSPITAL].n_total+=1;
+
 		if( gsl_ran_bernoulli( rng, model->params->critical_fraction[ indiv->age_group ] ) )
 		{
 			if( gsl_ran_bernoulli( rng, model->params->location_death_icu[ indiv->age_group ] ) )
@@ -385,6 +389,7 @@ void transition_to_critical( model *model, individual *indiv )
 {
 	set_critical( indiv, model->params, model->time );
 
+
 	if( model->params->hospital_on )
 	{
 		remove_if_in_waiting_list(indiv, &model->hospitals[indiv->hospital_idx]);
@@ -392,6 +397,9 @@ void transition_to_critical( model *model, individual *indiv )
 	}
 	else
 	{
+		model->event_lists[TRANSITION_TO_CRITICAL].n_daily_current[model->time]+=1;
+		model->event_lists[TRANSITION_TO_CRITICAL].n_total+=1;
+
 		if( gsl_ran_bernoulli( rng, model->params->fatality_fraction[ indiv->age_group ] ) )
 			transition_one_disese_event( model, indiv, CRITICAL, DEATH, CRITICAL_DEATH );
 		else
