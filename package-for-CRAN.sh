37d4bc7cc4323b17c54291e56580b4178fcaab57
Oli Legat <olegat@google.com>
2021-03-05 23:53:43 +0000

    Merge files /src/Makefile and /Makefile
    - Delete phony targets Rbuild, Rinstall, Rman and Rcheck
    - Update src/setup.py to use `gsl-config` to configure CFLAGS and LDFLAGS
    - Update README instructions to reflect new workflow
    - Remove a hack in package-for-CRAN.sh

--- package-for-CRAN.sh
+++ package-for-CRAN.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 # Initialize all generated files and run `R CMD build`.
 #
-
-# src/Makefile confuses Roxygen. Remove it temporarily.
-if [[ `git status --porcelain src/Makefile` ]]; then
-    echo 'src/Makefile has uncommitted changes. Aborting.' > /dev/stderr
-    exit 1
-else
-    rm src/Makefile
-fi
-
 autoconf
 make Rswig
 R -e 'devtools::document()'
 R CMD build .
-
-git checkout src/Makefile
